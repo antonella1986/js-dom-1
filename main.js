b6d54a1a9e215215f51fe217d32647a8533d8a56
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-18 18:20:04 +0100

    add variable

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /* Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato) */
 
-const button = 
+let button = document.getElementById("buttonLight");
 
-document.getElementById('buttonLight').addEventListener('click', function() {
-    document.getElementById('lighton').src = './img/yellow_lamp.png';
+button.addEventListener("click", function() {
+    document.getElementById("buttonLight").src = './img/yellow_lamp.png';
 });
