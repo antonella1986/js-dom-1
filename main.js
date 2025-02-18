73f0de56e358c16663c4a9db7d48996bc6f6c640
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-18 18:15:50 +0100

    divide files

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 /* Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato) */
 
+const button = 
+
+document.getElementById('buttonLight').addEventListener('click', function() {
+    document.getElementById('lighton').src = './img/yellow_lamp.png';
+});
